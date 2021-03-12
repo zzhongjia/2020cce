0eb50ceddaa4c44e1f39231a25a004d42c77be61
zzhongjia <79676928+zzhongjia@users.noreply.github.com>
2021-03-12 14:01:43 +0800

    Add files via upload

---
+++ Untitled1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int a[5]={0,10,20,30,40};
+void printfAll(){
+for(int i=0;i<5;i++)printf("%d ",a[i]);
+   printf("\n");
+   }
+ int main()
+ {
+
+     printfAll();
+
+     int* p=&a[2];
+     *p=222;
+     printfAll();
+     p=p+2;
+     *p=666;
+     printfAll();
+ }
+
